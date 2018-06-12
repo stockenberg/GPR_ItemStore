a8d66eb724e465f99c61f5bc39cc287a321fa1ec
Marten Stockenberg <MStockenberg@googlemail.com>
2018-06-12 11:27:52 +0200

    login page

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <template>
-  <div id="app">
-    <router-link to="/login">Login</router-link>
-    <router-link to="/shop">Shop</router-link>
-    <router-link to="/cart">Warenkorb</router-link>
-    <router-view/>
-  </div>
+    <div id="app">
+        <router-link to="/login">Login</router-link>
+        <router-link to="/shop">Shop</router-link>
+        <router-link to="/cart">Warenkorb</router-link>
+        <router-view/>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+    export default {
+        name: 'App'
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,15 +1,61 @@
 <template>
     <div>
-        <p>Login</p>
+        <form class="form-signin">
+            <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+            <label for="inputEmail" class="sr-only">Email address</label>
+            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="" data-cip-id="inputEmail">
+            <label for="inputPassword" class="sr-only">Password</label>
+            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="" data-cip-id="inputPassword">
+            <div class="checkbox mb-3">
+                <label>
+                    <input type="checkbox" value="remember-me"> Remember me
+                </label>
+            </div>
+            <button @click.prevent="alertMe" class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+            <p class="mt-5 mb-3 text-muted">© 2017-2018</p>
+        </form>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Login"
+        name: "Login",
+        methods: {
+            alertMe() {
+                alert('Hallo');
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+    .form-signin {
+        width: 100%;
+        max-width: 330px;
+        padding: 15px;
+        margin: auto;
+    }
+    .form-signin .checkbox {
+        font-weight: 400;
+    }
+    .form-signin .form-control {
+        position: relative;
+        box-sizing: border-box;
+        height: auto;
+        padding: 10px;
+        font-size: 16px;
+    }
+    .form-signin .form-control:focus {
+        z-index: 2;
+    }
+    .form-signin input[type="email"] {
+        margin-bottom: -1px;
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
+    }
+    .form-signin input[type="password"] {
+        margin-bottom: 10px;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+    }
 </style>
