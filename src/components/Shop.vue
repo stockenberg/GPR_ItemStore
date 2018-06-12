d34019990ef64e1d6feaee02f15260bb6b1ecbde
Marten Stockenberg <MStockenberg@googlemail.com>
2018-06-12 11:58:24 +0200

    added cart

--- src/components/Shop.vue
+++ src/components/Shop.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="container">
         <div class="jumbotron">
             <h1 class="display-4">Hello, world!</h1>
             <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content
