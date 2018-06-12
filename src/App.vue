ddaed076257be284209555232131f97d8cd1cd68
Marten Stockenberg <MStockenberg@googlemail.com>
2018-06-12 11:55:00 +0200

    added shop page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,30 @@
 <template>
     <div id="app">
-        <router-link to="/login">Login</router-link>
-        <router-link to="/shop">Shop</router-link>
-        <router-link to="/cart">Warenkorb</router-link>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <router-link to="/cart" class="nav-link">Warenkorb</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link to="/login" class="nav-link">Login</router-link>
+
+                        </li>
+                        <li class="nav-item">
+                            <router-link to="/shop" class="nav-link">Shop</router-link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+
         <router-view/>
     </div>
 </template>
@@ -14,12 +36,4 @@
 </script>
 
 <style>
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
 </style>
